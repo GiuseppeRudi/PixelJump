d3aaf22d3c7d345b8676096683892ae6b8d1b214
giu20 <rudigiuseppe1@gmail.com>
2024-05-16 15:04:09 +0200

    verrolandia

--- src/model/Player.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Player  extends AbstractPlayer{
-
-
-    public Player(Position position, int filtroSizeRiga, int filtroSizeColonna) { //costruttore player
-        super(position,filtroSizeRiga,filtroSizeColonna); //richiama la super classe
-    }
-}
--- src/model/Settings.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Settings {
-    public final static int World_Size_Riga = 20; //grandezza riga della matrice Principale
-    public final static int World_Size_Colonna = 161; //grandezza colonna della matrice Principale
-    public final static int Filtro_Size_Riga = 20; //grandezza riga della matrice filtro
-    public final static int Filtro_Size_Colonna = 30; //grandezza riga della matrice filtro
-
-}
